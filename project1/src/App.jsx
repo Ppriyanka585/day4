a529141fa1e143058cae67abf0465f1ee15a18f2
Ppriyanka585 <ppriyankababu1@gmail.com>
2023-05-25 12:15:11 +0530

    firstcommit

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,23 +1,28 @@
+import React from 'react'
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Login from './pages/Login.jsx';
+import NavBar1 from './components/NavBar1.jsx'
 
-function App() {
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+
+
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
- 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <NavBar1/>
+      <Container>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact/>} />
+       </Routes>
+       </Container>
     </div>
-  );
+  )
 }
 
-export default App;
